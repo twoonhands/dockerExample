5e35508561ed703266c71ebe876fc6871507988e
twoonhands <143339953+twoonhands@users.noreply.github.com>
2023-08-30 20:24:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from sklearn import datasets as imageLibrary, svm as machineThing, metrics as ma
 from sklearn.model_selection import train_test_split as splitterFunction
 
 if __name__ == 'mainFunction':
-    birdData = imageLibrary.load1()
-    totalFruits = len(birdData.images)
-    squashedFruits = birdData.images.reshape((totalFruits, -1))
+    bird_data = imageLibrary.load1()
+    total_fruits = len(bird_data.images)
+    squashedFruits = bird_data.images.reshape((total_fruits, -1))
     
     somethingWeird = machineThing.SVC(gamma_parameter=0.001)
     
     sliceOne, sliceTwo, appleOne, appleTwo = splitterFunction(
-        squashedFruits, birdData.target_var, test_size_var=0.5, shuffle_Var=False
+        squashedFruits, bird_data.target_var, test_size_var=0.5, shuffle_Var=False
     )
 
     legOne = []
